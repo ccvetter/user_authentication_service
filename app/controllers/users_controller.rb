23518d0260e598b578b32bc70e6be0d67dd66a5e
ccvetter <vetrik77@gmail.com>
2020-10-23 19:57:43 -0400

    add user update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController 
-    skip_before_action :authenticate_request, only: :create
+    skip_before_action :authenticate_request #, only: :create
 
     def index
         users = User.all()
@@ -8,6 +8,18 @@ class UsersController < ApplicationController
 
     def create
         user = User.new(email: params["email"], password: params["password"])
-        render json: { message: "user created" }
+        user.save!
+        render json: { error: 'Could not create user' }, status: 401 unless user
+        render json: { user: user }
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        new_attributes = {}
+        new_attributes["name"] = params["name"] if params["name"]
+        new_attributes["email"] = params["email"] if params["email"]
+        new_attributes["password"] = params["password"] if params["password"]
+        user.update(new_attributes)
+        render json: { user: user }
     end
 end
